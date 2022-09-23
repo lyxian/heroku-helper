1f086a596887a78b19e2575ef43c293e3bfb5ee2
lyx <yxianlai@hotmail.com>
2022-09-24 00:59:55 +0800

    init/log-errors - add postError route, telebot sendMessage

--- app/bot.py
+++ app/bot.py
@@ -1,8 +1,10 @@
+import pendulum
+import requests
 import telebot
 import logging
 
 # from telebot.types import ReplyKeyboardMarkup, KeyboardButton
-from utils import getToken
+from utils import getToken, loadSecrets
 from vars import START_MESSAGE
 
 def createBot():
@@ -17,6 +19,42 @@ def createBot():
         # bot.send_message(message.chat.id, text, reply_markup=createMarkup())
 
     return bot
+
+class TelebotHelper:
+    def __init__(self):
+        self.token = getToken()
+        if self.token is None:
+            raise Exception('Invalid Telebot Token')
+        self.configVars = loadSecrets()
+        
+
+    def callTelegramAPI(self, method, params):
+        url = 'https://api.telegram.org/bot{}/{}'.format(self.token, method)
+        response = requests.post(url=url, params=params)
+        # print(response.json())
+        return response.json()
+
+    def sendMessage(self, message):
+        method = 'sendMessage'
+        responses = {}
+        for chatId in self.configVars['Telegram']['Admins']:
+            params = {
+                'chat_id': chatId,
+                'parse_mode': 'HTML',
+                'text': message
+            }
+            response = self.callTelegramAPI(method, params)
+            ### Convert chatId to username ###
+            responses[chatId] = response
+        return responses
+
+    def formatLogs(self, message):
+        # FORMAT
+        # [ <TIME> ] <APP>
+        # <LOGS>
+        dateTime, appName, logs = message
+
+        return f'<i>[{dateTime}]</i> <b>{appName}</b>\n{logs}'
 
 if __name__ == "__main__":
     bot = createBot()
